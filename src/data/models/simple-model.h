e4bbc9adcb75c4e2fecc9d3c38d5f79161adef04
Artem Sheptunov <sheptunov-aa@mail.ru>
2025-09-01 22:16:14 +0500

    refactor(odb): #16: comment out experimental code

--- src/data/models/simple-model.h
+++ src/data/models/simple-model.h
@@ -1,43 +1,43 @@
-#pragma once
+// #pragma once
 
-#include <string>
+// #include <string>
 
-#include <odb/core.hxx>
+// #include <odb/core.hxx>
 
-#pragma db model version(1, 2)
-#pragma db object table("person") schema("public")
-class person
-{
-public:
-  person (const std::string& first,
-          const std::string& last,
-          const std::string& gender,
-          const bool& simple_bool1,
-          const std::string& simple_string1,
-          unsigned short age);
+// #pragma db model version(1, 2)
+// #pragma db object table("person") schema("public")
+// class person
+// {
+// public:
+//   person (const std::string& first,
+//           const std::string& last,
+//           const std::string& gender,
+//           const bool& simple_bool1,
+//           const std::string& simple_string1,
+//           unsigned short age);
 
-  const std::string& first () const;
-  const std::string& last () const;
-  const std::string& gender () const;
-  const bool& simple_bool1 () const;
-  const std::string& simple_string1 () const;
+//   const std::string& first () const;
+//   const std::string& last () const;
+//   const std::string& gender () const;
+//   const bool& simple_bool1 () const;
+//   const std::string& simple_string1 () const;
 
-  unsigned short age () const;
-  void age (unsigned short);
+//   unsigned short age () const;
+//   void age (unsigned short);
 
-private:
-  person () {}
+// private:
+//   person () {}
 
-  friend class odb::access;
+//   friend class odb::access;
 
-  #pragma db id auto
-  unsigned long long id_;
+//   #pragma db id auto
+//   unsigned long long id_;
 
-  std::string first_;
-  std::string last_;
+//   std::string first_;
+//   std::string last_;
 
-  std::string gender_;
-  bool simple_bool1_;
-  std::string simple_string1_;
-  unsigned short age_;
-};
+//   std::string gender_;
+//   bool simple_bool1_;
+//   std::string simple_string1_;
+//   unsigned short age_;
+// };
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,37 @@
 #include "to-dos-api.h"
 #include <vector>
 #include <string>
-#include <orm/db.hpp>
-#include <orm/utils/configuration.hpp>
+// #include <orm/db.hpp>
+// #include <orm/utils/configuration.hpp>
 
 #include <drogon/HttpAppFramework.h>
-#include "controllers/simple-controller.h"
+// #include "controllers/simple-controller.h"
 
-using Orm::DB;
+// using Orm::DB;
 
-using ConfigUtils = Orm::Utils::Configuration;
+// using ConfigUtils = Orm::Utils::Configuration;
 
-using namespace Orm::Constants;
+// using namespace Orm::Constants;
 
 int main() {
-    auto manager = DB::create({
-    {"driver",             "QPSQL"},
-    {"host",               qEnvironmentVariable("DB_HOST", "127.0.0.1")},
-    {"port",               qEnvironmentVariable("DB_PORT", "5432")},
-    {"database",           qEnvironmentVariable("DB_DATABASE", "postgres")},
-    {"search_path",        qEnvironmentVariable("DB_SEARCHPATH", "public")},
-    {"username",           qEnvironmentVariable("DB_USERNAME", "postgres")},
-    {"password",           qEnvironmentVariable("DB_PASSWORD", "postgres")},
-    {"charset",            qEnvironmentVariable("DB_CHARSET", "utf8")},
-    {"timezone",           "UTC"},
-    /* Specifies what time zone all QDateTime-s will have, the overridden default is
-       the QTimeZone::UTC, set to the QTimeZone::LocalTime or
-       QtTimeZoneType::DontConvert to use the system local time. */
-    // {"qt_timezone",        QVariant::fromValue(QTimeZone::UTC)},
-    {"prefix",             ""},
-    {"prefix_indexes",     false},
-    {"options",            QVariantHash()},
-    });
+    // auto manager = DB::create({
+    // {"driver",             "QPSQL"},
+    // {"host",               qEnvironmentVariable("DB_HOST", "127.0.0.1")},
+    // {"port",               qEnvironmentVariable("DB_PORT", "5432")},
+    // {"database",           qEnvironmentVariable("DB_DATABASE", "postgres")},
+    // {"search_path",        qEnvironmentVariable("DB_SEARCHPATH", "public")},
+    // {"username",           qEnvironmentVariable("DB_USERNAME", "postgres")},
+    // {"password",           qEnvironmentVariable("DB_PASSWORD", "postgres")},
+    // {"charset",            qEnvironmentVariable("DB_CHARSET", "utf8")},
+    // {"timezone",           "UTC"},
+    // /* Specifies what time zone all QDateTime-s will have, the overridden default is
+    //    the QTimeZone::UTC, set to the QTimeZone::LocalTime or
+    //    QtTimeZoneType::DontConvert to use the system local time. */
+    // // {"qt_timezone",        QVariant::fromValue(QTimeZone::UTC)},
+    // {"prefix",             ""},
+    // {"prefix_indexes",     false},
+    // {"options",            QVariantHash()},
+    // });
 
     drogon::app().setLogLevel(trantor::Logger::kDebug);
     drogon::app().addListener("127.0.0.1",8080);
