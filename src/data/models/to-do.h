add9da7cb44f4efc569aaf83e9ea89560450e56c
Oleg Kl <oklyavlin@tourmalinecore.com>
2025-09-09 00:39:40 +0500

    feat(migration): #18: add todo model and migration

---
+++ src/data/models/to-do.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <ctime>
+#include <odb/core.hxx>
+
+#pragma db object table("todo")
+class ToDo
+{
+public:
+    ToDo() = default;
+    ToDo(const std::string& name, std::time_t createdAtUtc, std::time_t deletedAtUtc = 0)
+        : name_(name), createdAtUtc_(createdAtUtc), deletedAtUtc_(deletedAtUtc) {}
+
+    std::uint64_t id() const { return id_; }
+    const std::string& name() const { return name_; }
+    std::time_t createdAtUtc() const { return createdAtUtc_; }
+    std::time_t deletedAtUtc() const { return deletedAtUtc_; }
+
+    void name(const std::string& n) { name_ = n; }
+    void createdAtUtc(std::time_t t) { createdAtUtc_ = t; }
+    void deletedAtUtc(std::time_t t) { deletedAtUtc_ = t; }
+
+private:
+    friend class odb::access;
+
+    #pragma db id auto
+    std::uint64_t id_;
+
+    std::string name_;
+    #pragma db type("TIMESTAMP")
+    std::time_t createdAtUtc_;
+
+    #pragma db null
+    #pragma db type("TIMESTAMP")
+    std::time_t deletedAtUtc_;
+};
---
+++ src/data/models/to_do.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Column, BigInteger, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class ToDo(Base):
+    __tablename__ = "todo"
+
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String(255), nullable=False)
+    createdAtUtc = Column(DateTime, nullable=False)
+    deletedAtUtc = Column(DateTime, nullable=True)
+
