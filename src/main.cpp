bf39082e5a18421751cce1957d77b51a53864adf
Oleg Kl <oklyavlin@tourmalinecore.com>
2025-09-09 22:20:07 +0500

    fix(db): #18: update connection to postgresql

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
-#include "data/queries/todo-query.h"
+#include "data/queries/todo-queries.h"
+#include "data/commands/todo-commands.h"
 #include <vector>
 #include <string>
 #include <odb/pgsql/database.hxx>
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 int main() {
     try
     {
+        // TODO(https://github.com/TourmalineCore/to-dos-api-cpp/issues/22): parse that variables from .env or .env.dev 
         const std::string user     = "postgres";
-        const std::string password = "postgres";
-        const std::string db_name  = "postgres";
+        const std::string password = "admin";
+        const std::string db_name  = "to-dos-api-cpp_db";
         const std::string host     = "localhost";
         const unsigned    port     = 5432;
 
@@ -24,23 +28,31 @@ int main() {
             new odb::pgsql::database(conninfo)
         );
 
-        ToDoQuery todo_query(*db);
+        ToDoQueries todo_queries(*db);
+        ToDoCommands todo_commands(*db);
+        const std::time_t now_utc = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        uint64_t added_todo = todo_commands.create_todo("random_todo", now_utc);
+        std::cout << "Added TODO:" <<  added_todo << std::endl;
+        uint64_t soft_removed_todo = todo_commands.soft_delete_todo(added_todo);
+        std::cout << "Soft Removed TODO:" <<  soft_removed_todo << std::endl;
+        uint64_t removed_todo = todo_commands.delete_todo(added_todo);
+        std::cout << "Removed TODO:" <<  removed_todo << std::endl;
 
-        std::time_t now = std::time(nullptr);
-        todo_query.add_todo("Checking of ODB", now);
+        std::shared_ptr<std::vector<ToDo>> todos = todo_queries.get_all_todos();
+        std::shared_ptr<ToDo> todo = todo_queries.get_todo_by_id(1);
 
-        std::shared_ptr<ToDo> todo = todo_query.get_todo_by_id(1);
 
-        if (todo)
+        if (todos && todo)
         {
-            std::cout << "Found TODO:" << std::endl;
-            std::cout << "ID: " << todo->id() << std::endl;
-            std::cout << "Name: " << todo->name() << std::endl;
-            std::cout << "Created At (timestamp): " << todo->createdAtUtc() << std::endl;
+            std::cout << "Found TODOs:" <<  todos->size() << std::endl;
+            const std::time_t t = todo->createdAtUtc();
+            std::tm tm{};
+            gmtime_r(&t, &tm);
+            std::cout << "Found TODO id:" << todo->id() << " createdAtUtc: " << std::put_time(&tm, "%Y-%m-%d %H:%M:%S UTC") << std::endl;
         }
         else
         {
-            std::cout << "TODO with id=1 not found" << std::endl;
+            std::cout << "TODO or TODOs not founded" << std::endl;
         }
     }
     catch (const odb::exception& e)
