69b11c2df3fa56e708460ac866e889429d05c401
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2025-09-09 15:43:25 +0500

    refactor: #19: remove redundant code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,4 @@
-#include "to-dos-api.h"
-#include <string>
-#include <vector>
-
 int main()
 {
-    to_dos_api();
-
-    std::vector<std::string> vec;
-    vec.emplace_back("test_package");
-
-    to_dos_api_print_vector(vec);
+    return 0;
 }
