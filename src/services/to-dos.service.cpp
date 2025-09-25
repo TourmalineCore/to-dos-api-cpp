f72cf41c2c8d112c355361ceb03110dc2a944270
Oleg Kl <oklyavlin@tourmalinecore.com>
2025-09-25 11:25:19 +0500

    feat(service): #28: link service with db

--- src/services/to-dos.service.cpp
+++ src/services/to-dos.service.cpp
@@ -1,39 +1,55 @@
 #include "to-dos.service.h"
+#include <chrono>
+#include <ctime>
 
-void ToDoService::addToDo(const std::string description) {
-    _toDos.push_back({
-      _nextToDoId, 
-      description
-    });
+using std::string;
+using std::vector;
 
-    _nextToDoId++;
-}
-
-const std::vector<ToDoDTO>& ToDoService::getToDos() const {
-    return _toDos;
-}
-
-Json::Value ToDoService::toJson() const {
-    Json::Value json;
-    Json::Value toDosArray(Json::arrayValue);
-    
-    for (const auto& toDo : _toDos) {
-        toDosArray.append(toDo.toJson());
-    }
-    
-    json["toDos"] = toDosArray;
-
-    return json;
-}
-
-bool ToDoService::completeToDo(int id) {
-    for (auto it = _toDos.begin(); it != _toDos.end(); ++it) {
-        if (it->id == id) {
-            _toDos.erase(it);
-
-            return true;
+static vector<ToDoDTO> mapToDTOs(const std::shared_ptr<std::vector<ToDo>>& todos)
+{
+    vector<ToDoDTO> out;
+    out.reserve(todos ? todos->size() : 0);
+    if (todos)
+    {
+        for (const auto& t : *todos)
+        {
+            ToDoDTO dto;
+            dto.id = static_cast<int>(t.id());
+            dto.description = t.name();
+            out.push_back(std::move(dto));
         }
     }
+    return out;
+}
 
-    return false;
+void ToDoService::addToDo(const string description)
+{
+    const std::time_t now_utc = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    (void) _commands.create_todo(description, now_utc);
+}
+
+bool ToDoService::completeToDo(int id)
+{
+    (void) _commands.soft_delete_todo(id);
+    return true;
+}
+
+const vector<ToDoDTO> ToDoService::getToDos() const
+{
+    auto todos = _queries.get_all_todos();
+    return mapToDTOs(todos);
+}
+
+Json::Value ToDoService::toJson() const
+{
+    Json::Value json;
+    Json::Value toDosArray(Json::arrayValue);
+    auto todos = _queries.get_all_todos();
+    auto dtos = mapToDTOs(todos);
+    for (const auto& dto : dtos)
+    {
+        toDosArray.append(dto.toJson());
+    }
+    json["toDos"] = toDosArray;
+    return json;
 }
