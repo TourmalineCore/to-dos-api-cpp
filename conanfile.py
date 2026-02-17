e8f8fac17ffcbe1e33a9dc1ee41388fc2787b399
Oleg Kl <oklyavlin@tourmalinecore.com>
2026-02-17 18:01:33 +0500

    chore(build): #44: update drogon version
    drogon with 1.9.10 doesent build even for x86-64

--- conanfile.py
+++ conanfile.py
@@ -43,7 +43,7 @@ class to_dos_apiRecipe(ConanFile):
         cmake.install()
     
     def requirements(self):
-        self.requires("drogon/1.9.10")
+        self.requires("drogon/1.9.12")
         self.requires("gtest/1.14.0")
         self.requires("libodb/2.5.0@cppget/stable")
         self.requires("libodb-pgsql/2.5.0@cppget/stable")
