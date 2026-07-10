e8b01470895365475eecc6c08e4fcfa2bd52cbd8
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2026-07-10 10:02:51 +0500

    chore: #79: add missing class import for libodb reciep

--- deps/recipes/libodb/all/conanfile.py
+++ deps/recipes/libodb/all/conanfile.py
@@ -3,6 +3,7 @@ import textwrap
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import get, save, rmdir
+from conan.errors import ConanInvalidConfiguration
 
 
 class LibOdbConan(ConanFile):
